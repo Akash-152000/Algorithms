b7c43d49cc049fca6cb564bb07edb6d795b4f645
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-05-03 10:09:58 +0530

    Interpolation search

--- Interploation search.py
+++ Interploation search.py
@@ -1,17 +1,27 @@
-def interpolation(n,x,lo,hi):
-    pos=lo-int((x-n[lo])*(lo-hi)/(n[hi]-n[lo]))
-    if pos==x:
-        return n.index(pos)
-    elif x<pos:
-        hi=pos
-        interpolation(n,x,lo,hi)
-    else:
-        lo=pos
-        interpolation(n,x,lo,hi)
-
-
-n=list(map(int,input().split()))
+n=[10, 12, 13, 16, 18, 19, 20, 21,22, 23, 24, 33, 35, 42, 47]
 x=int(input("Enter the element to be searched: "))
 lo=0
 hi=len(n)-1
-print("Elment is at the index:",interpolation(n,x,lo,hi))
+pos=0
+while x>=n[lo] and x<=n[hi]:
+    if lo==hi:
+        if n[lo] == x:  
+                pos=lo
+                break
+        else:
+            pos=-1
+            break
+    
+    else:
+        pos  = lo + int(((float(hi - lo) / ( n[hi] - n[lo])) * ( x - n[lo])))
+        if n[pos]==x:
+            break
+        elif n[pos]<x:
+            lo=pos+1
+        else:
+            hi=pos-1
+
+
+print(pos)
+
+        
