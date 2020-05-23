b41b0503d8e48e68ab65d91e9d9ba19b0b131a35
Akash <57212963+Akash-152000@users.noreply.github.com>
2020-05-23 11:08:30 +0530

    Update exponential search.py

--- Searching algorithms/exponential search.py
+++ Searching algorithms/exponential search.py
@@ -1,10 +1,21 @@
+def binarySearch(arr,l,h,x):
+    if h>=l:
+        mid=int(l+(h-l)/2)
+        if arr[mid]==x:
+            return mid
+        if arr[mid]>x:
+            return binarySearch(arr,l,mid-1,x)
+        
+        return binarySearch(arr,mid+1,h,x)
+    return -1
+
 def search(arr,n,x):
     if arr[0]==x:
         return 0
     i=1
     while i<n and arr[i]<=x:
         i=i*2
-    
+    return binarySearch(arr,i/2,min(i,n),x)
 
 
 arr=[2, 3, 4, 10, 40]
